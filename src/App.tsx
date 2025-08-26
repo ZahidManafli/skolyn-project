60cc57372fb46b5f14c4e5f070c1e35328ff46cd
Kaan Can <kaan66can@windowslive.com>
2025-08-26 18:49:16 +0300

    Trustedby section added.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import BackgroundVideo from "./components/BackgroundVideo";
 import HeroSection from "./components/HeroSection";
+import TrustedBy from "./components/TrustedBy";
 
 const App: React.FC = () => {
   return (
@@ -13,6 +14,7 @@ const App: React.FC = () => {
       <main className="pt-14">
         <HeroSection />
       </main>
+      <TrustedBy/>
     </>
   );
 };
---
+++ src/components/TrustedBy.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import kobiaLogo from "../assets/img/Kobia.png";
+import uniLogo from "../assets/img/UniLogo.png";
+import onkologiaLogo from "../assets/img/Onkologia.png";
+import googleLogo from "../assets/img/GoogleLogo.png";
+
+const TrustedBy: React.FC = () => {
+  return (
+    <section className="w-full bg-white ">
+      <div className="mx-auto max-w-7xl boder-b  ">
+        <h2 className="text-start text-5xl font-bold text-[#030F4F]">
+          Trusted By Our Strategic Partners
+        </h2>
+
+        <div className="mt-12 grid grid-cols-1 gap-8 items-center justify-items-center md:grid-cols-3">
+          <img src={kobiaLogo} alt="KOBİ" className=" object-contain" />
+
+          <img
+            src={uniLogo}
+            alt="Azerbaycan Tibb Universiteti"
+            className=" object-contain"
+          />
+
+          <div className="flex flex-col items-center gap-20">
+            <img
+              src={onkologiaLogo}
+              alt="Milli Onkologiya Mərkəzi"
+              className="object-contain"
+            />
+            <img
+              src={googleLogo}
+              alt="Google for Startups"
+              className=" object-contain"
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default TrustedBy;
