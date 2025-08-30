2cdb8d05699bd8437968999e6cb3ef9ba09b31dd
Kaan Can <kaan66can@windowslive.com>
2025-08-30 18:03:14 +0300

    TrustedBy fixed.

--- src/components/TrustedBy.tsx
+++ src/components/TrustedBy.tsx
@@ -6,22 +6,22 @@ import googleLogo from "../assets/img/GoogleLogo.png";
 
 const TrustedBy: React.FC = () => {
   return (
-    <section className="w-full bg-white ">
-      <div className="mx-auto max-w-7xl boder-b  ">
-        <h2 className="text-start text-5xl font-bold text-[#030F4F] pt-10">
+    <section className="w-full bg-white">
+      <div className="mx-auto max-w-7xl ">
+        <h2 className="text-start text-3xl md:text-5xl font-bold text-[#030F4F] pt-10">
           Trusted By Our Strategic Partners
         </h2>
 
-        <div className="mt-12 grid grid-cols-1 gap-8 items-center justify-items-center md:grid-cols-3">
-          <img src={kobiaLogo} alt="KOBİ" className=" object-contain" />
+        <div className="mt-12 grid grid-cols-2 gap-8 items-center justify-items-center md:grid-cols-3">
+          <img src={kobiaLogo} alt="KOBİ" className="object-contain" />
 
           <img
             src={uniLogo}
             alt="Azerbaycan Tibb Universiteti"
-            className=" object-contain"
+            className="object-contain"
           />
 
-          <div className="flex flex-col items-center gap-20">
+          <div className="hidden md:flex flex-col items-center gap-20">
             <img
               src={onkologiaLogo}
               alt="Milli Onkologiya Mərkəzi"
@@ -30,9 +30,21 @@ const TrustedBy: React.FC = () => {
             <img
               src={googleLogo}
               alt="Google for Startups"
-              className=" object-contain"
+              className="object-contain"
             />
           </div>
+
+          <img
+            src={onkologiaLogo}
+            alt="Milli Onkologiya Mərkəzi"
+            className="object-contain md:hidden"
+          />
+
+          <img
+            src={googleLogo}
+            alt="Google for Startups"
+            className="object-contain md:hidden"
+          />
         </div>
       </div>
     </section>
